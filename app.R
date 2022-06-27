f5863e3f1ef6558f6622b3b82dfe220ebd783020
Jing Wang <jing.wang5@nih.gov>
2022-06-26 22:12:01 -0400

    Add files via upload

---
+++ app.R
@@ -0,0 +1,82 @@
+
+library(shiny)
+library(ggplot2)
+
+freqpoly <- function(x1, x2, binwidth = 0.1, xlim = c(-3, 3)) {
+  df <- data.frame(x = c(x1, x2),g = c(rep("x1", length(x1)), rep("x2", length(x2))))
+  ggplot(df, aes(x, colour = g)) +
+    geom_freqpoly(binwidth = binwidth, size = 1) +
+    coord_cartesian(xlim = xlim)}
+t_test <- function(x1, x2) {
+  test <- t.test(x1, x2)
+  sprintf("p value: %0.3f\n[%0.2f, %0.2f]",test$p.value, test$conf.int[1], test$conf.int[2]
+  )}
+
+set.seed(123)
+x1 <- rnorm(100, mean = 0, sd = 0.5)
+x2 <- rnorm(200, mean = 0.15, sd = 0.9)
+
+freqpoly(x1, x2)
+cat(t_test(x1, x2))
+
+
+
+
+
+ui <- fluidPage(
+  fluidRow(
+    column(4, 
+           "Distribution 1",
+           numericInput("n1", label = "n", value = 1000, min = 1),
+           numericInput("mean1", label = "µ", value = 0, step = 0.1),
+           numericInput("sd1", label = "??", value = 0.5, min = 0.1, step = 0.1)
+    ),
+    column(4, 
+           "Distribution 2",
+           numericInput("n2", label = "n", value = 1000, min = 1),
+           numericInput("mean2", label = "µ", value = 0, step = 0.1),
+           numericInput("sd2", label = "??", value = 0.5, min = 0.1, step = 0.1)
+    ),
+    column(4,
+           "Frequency polygon",
+           numericInput("binwidth", label = "Bin width", value = 0.1, step = 0.1),
+           sliderInput("range", label = "range", value = c(-3, 3), min = -5, max = 5)
+    )
+  ),
+  fluidRow(
+    column(9, plotOutput("hist")),
+    column(3, verbatimTextOutput("ttest"))
+  )
+)
+
+
+# server <- function(input, output, session) {
+#   output$hist <- renderPlot({
+#     x1 <- rnorm(input$n1, input$mean1, input$sd1)
+#     x2 <- rnorm(input$n2, input$mean2, input$sd2)
+#     
+#     freqpoly(x1, x2, binwidth = input$binwidth, xlim = input$range)
+#   }, res = 96)
+#   
+#   output$ttest <- renderText({
+#     x1 <- rnorm(input$n1, input$mean1, input$sd1)
+#     x2 <- rnorm(input$n2, input$mean2, input$sd2)
+#     
+#     t_test(x1, x2)
+#   })
+# }
+
+server <- function(input, output, session) {
+  x1 <- reactive(rnorm(input$n1, input$mean1, input$sd1))
+  x2 <- reactive(rnorm(input$n2, input$mean2, input$sd2))
+  
+  output$hist <- renderPlot({
+    freqpoly(x1(), x2(), binwidth = input$binwidth, xlim = input$range)
+  }, res = 96)
+  
+  output$ttest <- renderText({
+    t_test(x1(), x2())
+  })
+}
+
+shinyApp(ui, server)
